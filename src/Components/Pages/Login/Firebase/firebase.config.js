0b23d605a19b8d2e167dc866aede99819e9aa6f2
Yasin440 <eiasin440@gmail.com>
2021-11-15 10:58:13 +0600

    environment event

--- src/Components/Pages/Login/Firebase/firebase.config.js
+++ src/Components/Pages/Login/Firebase/firebase.config.js
@@ -1,15 +1,9 @@
 const firebaseConfig = {
-    // apiKey: process.env.REACT_APP_API_KEY,
-    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    // projectId: process.env.REACT_APP_PROJECT_ID,
-    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    // appId: process.env.REACT_APP_APP_ID
-    apiKey: "AIzaSyB0aLzc2W0uCF7VBU5gQUIOQb-pSVuq-4g",
-    authDomain: "world-class-car.firebaseapp.com",
-    projectId: "world-class-car",
-    storageBucket: "world-class-car.appspot.com",
-    messagingSenderId: "866637506804",
-    appId: "1:866637506804:web:c88de5c7d645ba098d16a1"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 export default firebaseConfig;
